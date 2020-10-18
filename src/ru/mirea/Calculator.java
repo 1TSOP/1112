862dacd1cb236f27e1d64aab3e56c06205f703c0
1TSOP <nikita.postnov01@mail.ru>
2020-10-18 14:36:06 +0300

    Initial commit

---
+++ src/ru/mirea/Calculator.java
@@ -0,0 +1,44 @@
+package ru.mirea;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class Calculator extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.getIcons().add(new Image(Calculator.class.getResourceAsStream( "ico.png" )));
+
+        FXMLLoader loader = new FXMLLoader();
+        URL xmlUrl = getClass().getResource("calc.fxml");
+        loader.setLocation(xmlUrl);
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Калькулятор");
+        primaryStage.setHeight(280);
+
+        primaryStage.show();
+    }
+}
